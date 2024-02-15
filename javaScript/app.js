2a5aacd34e87e075ec2c37d070cbae9ad03e24be
impactgalaxy <traderglow@gmail.com>
2024-02-15 15:57:07 +0600

    added mobile feature

--- javaScript/app.js
+++ javaScript/app.js
@@ -5,12 +5,39 @@ const YourLife = document.getElementById("your-life");
 let score = 0;
 let life = 5;
 
-function handleKeyUp(event) {
-    const userKey = event.key;
+// KeyboardEvent
+// function handleKeyUp(event) {
+//     const userKey = event.key;
+//     console.log(userKey, "by keyboard");
+//     const displayKey = document.getElementById("output").innerText.toLowerCase();
+
+//     if (userKey === displayKey) {
+//         console.log("You got a point");
+//         gamingLoop();
+//         removeBackground(displayKey);
+//         score += 1;
+//         yourScore.innerText = score;
+//         finalScore.innerText = score;
+//     }
+//     else {
+//         life -= 1;
+//         if (life === 0) {
+//             gameOver(displayKey);
+//         }
+//         YourLife.innerText = life;
+//     }
+//     if (userKey === "Escape") {
+//         gameOver();
+//     }
+// }
+
+function mobileView(event) {
+    const userKey = event.target.value;
+    console.log(userKey, "by input");
     const displayKey = document.getElementById("output").innerText.toLowerCase();
 
     if (userKey === displayKey) {
-        console.log("You got a point");
+        //console.log("You got a point");
         gamingLoop();
         removeBackground(displayKey);
         score += 1;
@@ -27,11 +54,15 @@ function handleKeyUp(event) {
     if (userKey === "Escape") {
         gameOver();
     }
-}
 
-window.addEventListener("keyup", handleKeyUp);
+}
+//window.addEventListener("keyup", handleKeyUp);
 const inputForMobile = document.getElementById("inputForMobile");
 inputForMobile.addEventListener("keyup", mobileView);
+inputForMobile.addEventListener("keyup", function (event) {
+    event.target.value = "";
+});
+
 
 
 function gameOver(r) {
@@ -63,27 +94,13 @@ function againPlay() {
 function home() {
     location.reload();
 }
-function mobileView(event) {
-    const userKey = event.key;
-    const displayKey = document.getElementById("output").innerText.toLowerCase();
 
-    if (userKey === displayKey) {
-        console.log("You got a point");
-        gamingLoop();
-        removeBackground(displayKey);
-        score += 1;
-        yourScore.innerText = score;
-        finalScore.innerText = score;
-    }
-    else {
-        life -= 1;
-        if (life === 0) {
-            gameOver(displayKey);
-        }
-        YourLife.innerText = life;
-    }
-    if (userKey === "Escape") {
-        gameOver();
-    }
+
+console.log(window.innerWidth);
+if (window.innerWidth < 600) {
+
+
+}
+else {
 
 }
